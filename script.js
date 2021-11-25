5e60f9a0eca9fdf72103b63d4be540a075b884a4
dmkirshon <kirshon.david@gmail.com>
2021-11-25 18:10:19 +0200

    feat: Implement function to play one round

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let playerSelection;
 let computerSelection;
 let gameStatus;
 
-// computerPlay player will play against a computer that will randomly return either rock, paper, scissors
+// computerPlay returns a random choice of rock, paper or scissors
 function computerPlay() {
     const randomValue = Math.floor(Math.random() * 3) + 1;
 
@@ -22,4 +22,23 @@ function computerPlay() {
 
 // playRound function which will play through one round, will take in playerSelection and computerSelection, return string declaring winner
 
+function playRound(playerSelection, computerSelection) {
+    const winningPhrase = `You win: ${playerSelection} beats ${computerSelection}!`;
+    const losingPhrase = `You lose: ${computerSelection} beats ${playerSelection}!`;
+    const tiePhrase = `You tied! Both of you picked ${playerSelection}`
+
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return winningPhrase;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return winningPhrase;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return winningPhrase;
+    } else if (playerSelection === computerSelection) {
+        return tiePhrase;
+    } else {
+        return losingPhrase;
+    }
+}
+
+console.log(playRound("rock",computerPlay()));
 // create a game running through playRound 5 times, keeps score and reports a winner
